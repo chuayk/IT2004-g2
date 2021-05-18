def3579e33ed9e5bfd2d4ef243ba33bf7fbc7435
yihang831 <2549277623zyh@gmail.com>
2021-05-18 11:45:32 +0800

    walkInCustomer

--- routes/staff.mjs
+++ routes/staff.mjs
@@ -139,6 +139,7 @@ router.post("/createcode",async function(req,res){
 //         console.log('Oops! something went wrong, : ', err);
 //      });
 //});
+
 //create walk in user -yh
 router.get("/createWalkInUser",      async function(req, res) {
 	console.log("create walk in user page accessed");
